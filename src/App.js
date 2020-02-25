5748f0cebd6257a8474b3b7dc152a0d739cd1c76
ngerwig <nancy.gerwig@vertexinc.com>
2020-02-25 14:30:54 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    Hi There you I hate you!!!!
+                    I don't understand why this works but it does
                 </p>
                 <a
                     className="App-link"
