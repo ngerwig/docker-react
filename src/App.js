05c7845319836f21ee043db0601d6b0177f22b44
ngerwig <nancy.gerwig@VERTEXINC.COM>
2020-02-26 10:11:04 -0500

    update app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    I don't understand why this works but it does
+                    Changed on feature
                 </p>
                 <a
                     className="App-link"
